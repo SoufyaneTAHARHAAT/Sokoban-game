4c7d6d03aa0bdc51eb9364c54e5ce2d9ccfd12bb
SoufyaneTAHARHAAT <soufyanetahar@gmail.com>
2024-03-23 13:08:48 +0100

    ajout de la classe CaseObjectif et positionnement dans la grille, des modifications encore à faire

---
+++ src/modele/CaseObjectif.java
@@ -0,0 +1,10 @@
+package modele;
+
+public class CaseObjectif extends Vide {
+    public CaseObjectif(Jeu _jeu) {super(_jeu);}
+
+    @Override
+    public boolean peutEtreParcouru() {
+        return false;
+    }
+}
