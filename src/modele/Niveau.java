b3b99923a017394e6e11d76b61cf164c16e55e25
MaheryRAVELOSON <jonathan.mahery02@gmail.com>
2024-04-06 09:22:36 +0200

    Rendre la nouvelle fichier Niveau7.txt utilisable pour l'itération dans la classe Niveau

--- src/modele/Niveau.java
+++ src/modele/Niveau.java
@@ -6,7 +6,7 @@ public class Niveau {
 
     public Niveau(){
         IndiceNiveauActuel = -1;
-        int nombreNiveaux = 7;
+        int nombreNiveaux = 8;
         TabNiveau = new String[nombreNiveaux];
         for (int i = 0; i < nombreNiveaux; i++) {
             TabNiveau[i] = "Niveaux/Niveau" + i + ".txt";
